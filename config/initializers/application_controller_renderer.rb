5c01a6c3165a18c9a04c20c09385cb1958318154
houmitsu <fumiya.houmitsu1028@gmail.com>
2021-07-02 05:54:35 +0000

    画像投稿機能作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'fa8bd497b2308d3cb6d9af676bf3484adeb8d8db51370ca9a5136ee26e112a1ee477af85ec08622730f25470866ffbd72efa30f365441cb351c8c859dcee6934'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_122220) do
+ActiveRecord::Schema.define(version: 2021_07_02_052311) do
+
+  create_table "post_images", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
